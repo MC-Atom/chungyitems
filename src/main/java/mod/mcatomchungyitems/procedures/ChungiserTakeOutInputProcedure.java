126dc9949551e9ccd44c391c012f179581cbe1a2
Melon <56051776+melonboy10@users.noreply.github.com>
2021-05-17 20:25:42 -0700

    Update and rename ChungusTakeOutInputProcedure.java to ChungiserTakeOutInputProcedure.java

--- src/main/java/mod/mcatomchungyitems/procedures/ChungiserTakeOutInputProcedure.java
+++ src/main/java/mod/mcatomchungyitems/procedures/ChungiserTakeOutInputProcedure.java
@@ -5,11 +5,12 @@ import mod.mcatomchungyitems.ChungyitemsModElements;
 import java.util.Map;
 
 @ChungyitemsModElements.ModElement.Tag
-public class ChungusTakeOutInputProcedure extends ChungyitemsModElements.ModElement {
-	public ChungusTakeOutInputProcedure(ChungyitemsModElements instance) {
+public class ChungiserTakeOutInputProcedure extends ChungyitemsModElements.ModElement {
+	public ChungiserTakeOutInputProcedure(ChungyitemsModElements instance) {
 		super(instance, 8);
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
+		System.out.println("HA YOUR MUM HAPPY LEL!");
 	}
 }
